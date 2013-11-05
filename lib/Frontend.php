26be8e4da7570456031ec415db4454ad94f04b9d
herizo <herizo.ludovic@gmail.com>
2013-11-05 11:34:24 +0300

    first standard elements

---
+++ lib/Frontend.php
@@ -0,0 +1,17 @@
+<?php
+
+class Frontend extends ApiFrontend {
+  function init(){
+    parent::init();
+
+    $this->dbConnect();
+
+    $this->addLocation('atk4-addons',array(
+                'php'=>array(
+                    'mvc',
+                    'misc/lib',
+                    ))
+
+    )->setParent($this->pathfinder->base_location);
+  }
+}
